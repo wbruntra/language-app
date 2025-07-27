86afb68a93c8d40744168cac0b4d9d4f2f3640f0
William Bruntrager <w.bruntrager@gmail.com>
2025-07-27 21:05:29 +0200

    Remove log

--- backend/db_connection.js
+++ backend/db_connection.js
@@ -3,8 +3,6 @@ const knexfile = require('./knexfile')
 
 const config = knexfile[process.env.NODE_ENV || 'development']
 
-console.log('Using Knex configuration:', config)
-
 const db = knex(config)
 
 module.exports = db
